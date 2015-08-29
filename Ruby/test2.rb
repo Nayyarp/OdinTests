e0d5b22c125469ed47273a12ab9e70b56ea62867
Raju <nayyarp@gmail.com>
2015-08-28 23:59:04 -0400

    Methods and Branching commands

---
+++ Ruby/test2.rb
@@ -0,0 +1,22 @@
+#Ruby Methods
+var1 = 'stop'
+var2 = 'stressed'
+var3 = 'latveria'
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+
+puts var1
+puts var2
+puts var3
+
+#String Length
+puts 'What is your name?'
+name = gets.chomp
+puts 'Cool! There are ' + name.length.to_s + ' letters in your name, ' + name + '!'
+
+#Centring
+lineWidth = 40
+puts( 'Hey little cheerio'.center(lineWidth))
+puts( 'Zip bibble dee beerio'.center(lineWidth))
+
