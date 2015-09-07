dac5295fa21a744c99ba4b0321ee1bd4208b1ba4
Raju <nayyarp@gmail.com>
2015-09-06 20:41:26 -0400

    Following Lynda tutorials

---
+++ Ruby/booleans.rb
@@ -0,0 +1,10 @@
+#Flow Control Conditionals
+
+puts 'Please enter your name'
+name = gets.chomp
+if name == 'Kevin'.downcase 
+	puts 'Hello Kevin'
+else 
+	puts 'Oh, hello...'
+end
+
---
+++ Ruby/lyndatests/conditionals.rb
@@ -0,0 +1,9 @@
+#Flow Control Conditionals
+
+puts 'Please enter your name'
+name = gets.chomp
+if name == 'Kevin'.downcase 
+	puts 'Hello Kevin'
+else 
+	puts 'Oh, hello...'
+end
